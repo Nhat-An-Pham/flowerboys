1832c916e37a98dade694a4bd5d02684f8699ca3
minhnhut571 <minhnhut05072001@gmail.com>
2023-02-13 00:47:30 +0700

    API
    API v1

---
+++ Traibanhoa/Traibanhoa/Modules/BasketModule/BasketService.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Traibanhoa.Modules.TypeModule.Interface;
+using Traibanhoa.Modules.TypeModule.Request;
+using Traibanhoa.Modules.TypeModule.Response;
+using Traibanhoa.Modules.BasketModule.Interface;
+using Models.Models;
+using Traibanhoa.Modules.BasketModule.Request;
+using Type = Models.Models.Type;
+using Traibanhoa.Modules.TypeModule;
+
+namespace Traibanhoa.Modules.BasketModule
+{
+    public class BasketService : IBasketService
+    {
+        private readonly IBasketRepository _BasketRepository;
+        private readonly ITypeRepository _typeRepository;
+        public BasketService(IBasketRepository BasketRepository, ITypeRepository typeRepository)
+        {
+            _BasketRepository = BasketRepository;
+            _typeRepository = typeRepository;
+        }
+
+        public async Task<ICollection<Basket>> GetAll()
+        {
+            return await _BasketRepository.GetAll();
+        }
+
+        public Task<ICollection<Basket>> GetBasketsBy(
+                Expression<Func<Basket,
+                bool>> filter = null,
+                Func<IQueryable<Basket>,
+                ICollection<Basket>> options = null,
+                string includeProperties = null)
+        {
+            return _BasketRepository.GetBasketsBy(filter);
+        }
+
+
+        public async Task<Boolean> AddNewBasket(CreateBasketRequest BasketRequest)
+        {
+            var newBasket = new Basket();
+
+            if(_BasketRepository.GetFirstOrDefaultAsync(x => x.BasketId == BasketRequest.BasketId) == null)
+            {
+                return false;
+            }
+            newBasket.BasketId= Guid.NewGuid();
+            newBasket.Title = BasketRequest.Title;
+            newBasket.Description = BasketRequest.Description;
+            newBasket.ImageUrl = BasketRequest.ImageUrl;
+            newBasket.View = BasketRequest.View;
+            newBasket.BasketPrice = BasketRequest.BasketPrice;
+            newBasket.Status = BasketRequest.Status;
+            newBasket.CreatedDate = DateTime.Now;
+            newBasket.UpdatedDate = null;
+
+            await _BasketRepository.AddAsync(newBasket);
+            return true;
+        }
+
+        public async Task<Boolean> UpdateBasket(UpdateBasketRequest BasketRequest)
+        {
+            var BasketUpdate = GetBasketByID(BasketRequest.BasketId).Result;
+
+            BasketUpdate.Title = BasketRequest.Title;
+            BasketUpdate.Description = BasketRequest.Description;
+            BasketUpdate.ImageUrl = BasketRequest.ImageUrl;
+            BasketUpdate.View = BasketRequest.View;
+            BasketUpdate.BasketPrice = BasketRequest.BasketPrice;
+            BasketUpdate.Status = BasketRequest.Status;
+            BasketUpdate.UpdatedDate = DateTime.Now;
+
+            await _BasketRepository.UpdateAsync(BasketUpdate);
+            return true;
+        }
+
+        public async Task<Boolean> DeleteBasket(Basket BasketDelete)
+        {
+            BasketDelete.Status = 0;
+            await _BasketRepository.UpdateAsync(BasketDelete);
+            return true;
+        }
+
+        public async Task<Basket> GetBasketByID(Guid? id)
+        {
+            return await _BasketRepository.GetFirstOrDefaultAsync(x => x.BasketId == id);
+        }
+
+        //public async Task<ICollection<TypeDropdownResponse>> GetTypeDropdown()
+        //{
+        //    var result = await _typeRepository.GetTypesBy(x => x.Status == true);
+        //    return result.Select(x => new TypeDropdownResponse
+        //    {
+        //        TypeId = x.TypeId,
+        //        TypeName = x.Name
+        //    }).ToList();
+        //}
+    }
+}
---
+++ Traibanhoa/Traibanhoa/Modules/CustomerModule/Request/CreateCustomerRequest.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System;
+using Traibanhoa.Modules.TypeModule.Request;
+
+namespace Traibanhoa.Modules.CustomerModule.Request
+{
+    public class CreateCustomerRequest
+    {
+        public Guid CustomerId { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phonenumber { get; set; }
+        public bool? Gender { get; set; }
+        public string Avatar { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool? IsGoogle { get; set; }
+        public bool? IsBlocked { get; set; }
+    }
+    public class CreateCustomerRequestValidator : AbstractValidator<CreateCustomerRequest>
+    {
+        public CreateCustomerRequestValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty().NotNull();
+            RuleFor(x => x.Name).NotEmpty().NotNull();
+            RuleFor(x => x.Email).NotEmpty().NotNull();
+            RuleFor(x => x.Password).NotEmpty().NotNull();
+            RuleFor(x => x.Phonenumber).NotEmpty().NotNull();
+            RuleFor(x => x.Gender).NotEmpty().NotNull();
+            RuleFor(x => x.Avatar).NotEmpty().NotNull();
+            RuleFor(x => x.CreatedDate).NotEmpty().NotNull();
+            RuleFor(x => x.UpdatedDate).NotEmpty().NotNull();
+            RuleFor(x => x.IsGoogle).NotEmpty().NotNull();
+            RuleFor(x => x.IsBlocked).NotEmpty().NotNull();
+        }
+    }
+}
